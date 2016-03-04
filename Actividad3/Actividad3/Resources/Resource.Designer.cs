6d24b33f3785ce1e6e89036ce3df62882752d127
MAURICIO NEPOMUCENO DE FLORENCIO <mauricio41314@comunidad.unam.mx>
2016-03-03 20:39:06 -0600

    Saludos

--- Actividad3/Actividad3/Resources/Resource.Designer.cs
+++ Actividad3/Actividad3/Resources/Resource.Designer.cs
@@ -63,8 +63,35 @@ namespace Actividad3
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f060002
+			public const int MyButton = 2131099650;
+			
+			// aapt resource value: 0x7f060008
+			public const int absoluteLayout1 = 2131099656;
+			
+			// aapt resource value: 0x7f060003
+			public const int button1 = 2131099651;
+			
+			// aapt resource value: 0x7f060005
+			public const int button2 = 2131099653;
+			
+			// aapt resource value: 0x7f060001
+			public const int linearLayout1 = 2131099649;
+			
 			// aapt resource value: 0x7f060000
-			public const int MyButton = 2131099648;
+			public const int textView1 = 2131099648;
+			
+			// aapt resource value: 0x7f060009
+			public const int toolbar1 = 2131099657;
+			
+			// aapt resource value: 0x7f060007
+			public const int zoomButton1 = 2131099655;
+			
+			// aapt resource value: 0x7f060004
+			public const int zoomButton2 = 2131099652;
+			
+			// aapt resource value: 0x7f060006
+			public const int zoomButton3 = 2131099654;
 			
 			static Id()
 			{
